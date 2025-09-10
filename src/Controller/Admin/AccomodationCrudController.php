48286fd26592c41b9aca686786c42d799d164d5a
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-09-10 16:32:49 +0200

    added basic admin crud controllers and linked them in dashboard

---
+++ src/Controller/Admin/AccomodationCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Accomodation;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AccomodationCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Accomodation::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,20 +2,36 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Accomodation;
+use App\Entity\Announcement;
+use App\Entity\Convenience;
+use App\Entity\Image;
+use App\Entity\Message;
+use App\Entity\Reservation;
+use App\Entity\Review;
+use App\Entity\Service;
+use App\Entity\Unavailability;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
+        
         // Option 1. You can make your dashboard redirect to some common page of your backend
+
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
         //
         // 1.1) If you have enabled the "pretty URLs" feature:
         // return $this->redirectToRoute('admin_user_index');
@@ -44,7 +60,37 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        // Accueil & navigation générale
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fas fa-home');
+
+        yield MenuItem::linkToRoute("Retour à l'accueil du site", 'fas fa-home', 'app_home')
+            ->setCssClass('mb-4');
+
+        // Offre & Contenus
+        yield MenuItem::section('Offre & Contenus');
+        yield MenuItem::linkToCrud('Annonces', 'fas fa-bullhorn', Announcement::class);
+        yield MenuItem::linkToCrud('Logements', 'fas fa-house', Accomodation::class);
+        yield MenuItem::linkToCrud('Images', 'fas fa-images', Image::class);
+
+        // Expérience & Qualité
+        yield MenuItem::section('Expérience & Qualité');
+        yield MenuItem::linkToCrud('Avis', 'fas fa-star', Review::class);
+        yield MenuItem::linkToCrud('Messages', 'fas fa-comments', Message::class);
+
+        // Planning & Opérations
+        yield MenuItem::section('Planning & Opérations');
+        yield MenuItem::linkToCrud('Réservations', 'fas fa-calendar-check', Reservation::class);
+        yield MenuItem::linkToCrud('Indisponibilités', 'fas fa-calendar-xmark', Unavailability::class);
+
+        // Paramétrages
+        yield MenuItem::section('Paramétrages');
+        yield MenuItem::linkToCrud('Conveniences (équipements)', 'fas fa-list-check', Convenience::class);
+        yield MenuItem::linkToCrud('Services', 'fas fa-screwdriver-wrench', Service::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+
+        // Outils
+        yield MenuItem::section('Outils');
+        // yield MenuItem::linkToLogout('Déconnexion', 'fas fa-right-from-bracket');
     }
 }
